61dd3aec01df5688225897556bbe45d5173d03e9
minseo <raichal2000@naver.com>
2024-03-24 22:27:27 +0900

    feat : add DOOD in shell script

--- airflow/airflow_for_worker/run_for_worker_node.sh
+++ airflow/airflow_for_worker/run_for_worker_node.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
-
-
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
    && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
    && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
@@ -12,6 +10,7 @@ docker run -d -it --restart=always --name worker1 -p 8080:8080 \
 -v ~/airflow_worker/dags:/root/airflow/dags \
 -v ~/airflow_worker/plugins:/root/airflow/plugins \
 -v ~/airflow_worker/log:/root/airflow/log \
+-v /var/run/docker.sock:/var/run/docker.sock \ 
 -v /etc/localtime:/etc/localtime:ro -e TZ=Asia/Seoul \
 airflow_worker:1.0 \
 airflow celery worker -H worker1 -q queue1
