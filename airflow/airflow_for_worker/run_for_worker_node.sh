1c86291e052c461cf05fb1314087e41c3f9042b5
minseo <raichal2000@naver.com>
2024-03-24 22:57:44 +0900

    feat : add DOOD

--- airflow/airflow_for_worker/run_for_worker_node.sh
+++ airflow/airflow_for_worker/run_for_worker_node.sh
@@ -5,12 +5,14 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
 sudo apt-get update
 sudo apt-get install -y nvidia-docker2
 sudo systemctl restart docker
+sudo apt -y install nfs-common
+sudo mkdir -p /home/nfs_server
+sudo mount 10.0.1.6:/home/airflow_for_master ~/airflow_worker
 docker build -t airflow_worker:1.0 .
 docker run -d -it --restart=always --name worker1 -p 8080:8080 \
 -v ~/airflow_worker/dags:/root/airflow/dags \
 -v ~/airflow_worker/plugins:/root/airflow/plugins \
 -v ~/airflow_worker/log:/root/airflow/log \
--v /var/run/docker.sock:/var/run/docker.sock \ 
--v /etc/localtime:/etc/localtime:ro -e TZ=Asia/Seoul \
+-v /var/run/docker.sock:/var/run/docker.sock -v /etc/localtime:/etc/localtime:ro -e TZ=Asia/Seoul \
 airflow_worker:1.0 \
 airflow celery worker -H worker1 -q queue1
