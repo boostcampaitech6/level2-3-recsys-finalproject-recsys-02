f1d554409d79b6a1b548e58a6500480a04640420
ChangZero <changzero.kim@gmail.com>
2024-03-24 11:22:05 +0900

    Feat: storage config

--- servingAPI/utils/config.py
+++ servingAPI/utils/config.py
@@ -1,1 +1,9 @@
-##secrets##
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+class Config(BaseSettings):
+    storage_path: str = Field(default="./storage", env="MODEL_PATH")
+    # app_env: str = Field(default="local", env="APP_ENV")
+    
+
+config = Config()
